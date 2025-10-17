81460f2769cab660876ce3c7d7056bf59a89fe8a
kamarajdurai <Kamarajdurai2005@gmail.com>
2025-10-17 08:25:42 +0530

    modified:   src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,4 @@
 import "../styles/App.css";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <section className="home-container">
@@ -11,14 +9,6 @@ export default function Home() {
         <p>Discover modern tech gadgets at unbeatable prices.</p>
         <button className="shop-btn">Explore Products</button>
       </div>
-      <Image
-  src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?auto=format&fit=crop&w=900&q=60"
-  alt="Shopping Banner"
-  className="home-banner"
-  width={900}
-  height={600}
-  priority
-/>
     </section>
   );
 }
